bcaffbc5d89fe5d4ef545efa944ba827a32b26d9
Michal Migurski <mike@teczno.com>
2020-09-20 11:56:33 -0700

    Updated inference to accept image literals and return JSON inferences

--- demo/inference.py
+++ demo/inference.py
@@ -1,34 +1,34 @@
 from mxnet.image import imdecode
-from gluoncv import model_zoo, data, utils
+from gluoncv import model_zoo, data
 import requests
-from io import BytesIO
-import json
 import base64
 
 net = model_zoo.get_model('yolo3_mobilenet1.0_coco', pretrained=True, root='/tmp/')
 
 def lambda_handler(event, context):
     try:
-        print('Trying')
-        print('event:', event)
-        url = event['img_url']
-        response = requests.get(url)
-        img = imdecode(response.content)
-        x, img = data.transforms.presets.yolo.transform_test([img], short=540)
+        try:
+            url = event['img_url']
+            response = requests.get(url)
+            img = imdecode(response.content)
+            print('Read {1}x{0} image from event["img_url"]'.format(*img.shape))
+        except KeyError:
+            a85 = event['img_a85']
+            img = imdecode(base64.a85decode(a85))
+            print('Read {1}x{0} image from event["img_a85"]'.format(*img.shape))
+
+        x, _ = data.transforms.presets.yolo.transform_test([img], short=540)
         class_IDs, scores, bounding_boxs = net(x)
-        print('class_IDs.shape:', class_IDs.shape)
-        print('scores.shape:', scores.shape)
-        print('bounding_boxs.shape:', bounding_boxs.shape)
-        print('net.classes:', net.classes)
-        for i in range(10):
-            class_ID, score, bounding_box = int(class_IDs[0,i,0].asscalar()), scores[0,i,0].asscalar(), bounding_boxs[0,i,:].asnumpy()
-            if class_ID != -1:
-                print(i, net.classes[class_ID], score, bounding_box)
-        output = utils.viz.plot_bbox(img, bounding_boxs[0], scores[0],
-                            class_IDs[0], thresh=.3, class_names=net.classes)
-        output.axis('off')
-        f = BytesIO()
-        output.figure.savefig(f, format='jpeg', bbox_inches='tight')
-        return base64.b64encode(f.getvalue())
+        
+        return [
+            {
+                'class': net.classes[int(class_IDs[0,i,0].asscalar())],
+                'score': round(float(scores[0,i,0].asscalar()), 3),
+                'bounds': [int(n) for n in bounding_boxs[0,i,:].asnumpy()],
+            }
+            for i in range(class_IDs.shape[1])
+            if scores[0,i,0].asscalar() > .3
+            ]
+
     except Exception as e:
         raise Exception(f'ProcessingError: {e}')
